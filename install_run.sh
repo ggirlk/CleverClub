d0eb3a704f6893630dca49b342d71dd7a296f892
khouloud <ggirlk2@gmail.com>
2024-08-12 12:37:14 +0100

    add sudo

--- install_run.sh
+++ install_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build the Docker images Run the Docker containers
-docker-compose up -d --build
+sudo docker-compose up -d --build
 
-docker-compose run web python manage.py makemigrations
+sudo docker-compose run web python manage.py makemigrations
 
-docker-compose run web python manage.py migrate
+sudo docker-compose run web python manage.py migrate
 
 gunzip < clubdb_dump.sql.gz | sudo docker exec -i cleverclub-db-1 psql -U root -d clubdatabase
